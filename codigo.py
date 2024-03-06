d8eea2f56e4d13359adc1b14fd4bb03cad0ca8f5
Gusthavo Rodrigues <161639902+GusthavoRA@users.noreply.github.com>
2024-03-06 15:51:07 -0300

    automação login
    automação que abre o cliente, faz login e abre o jogo

---
+++ codigo.py
@@ -0,0 +1,38 @@
+#passo a passo do projeto
+
+import pyautogui 
+import time
+
+pyautogui.PAUSE = 1
+
+#minimizar vs
+pyautogui.click(x=1806, y=15)
+time.sleep (1.5)
+
+#abrir cliente riot 
+pyautogui.press("win")
+pyautogui.write("cliente riot")
+pyautogui.press("enter")
+time.sleep (5)
+
+
+#fazer login 
+pyautogui.click(x=299, y=372)
+pyautogui.write("meulogin")
+
+#senha
+pyautogui.press("tab")
+pyautogui.write("minhasenha")
+time.sleep(1)
+
+#logar
+pyautogui.click(x=392, y=778)
+time.sleep(5)
+
+#selecionar valorant
+pyautogui.click(x=437, y=454)
+time.sleep(3)
+
+#play 
+pyautogui.click(x=351, y=409)
+
